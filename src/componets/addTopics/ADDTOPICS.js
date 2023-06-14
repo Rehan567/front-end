bc57166b9c5a8225d437a6cbcd246639b95d9191
Md Rehan <rehanmohd042@gmail.com>
2023-06-14 13:05:35 +0530

    statement

--- src/componets/addTopics/ADDTOPICS.js
+++ src/componets/addTopics/ADDTOPICS.js
@@ -1,81 +1,85 @@
-import React, { useState } from 'react'
-import { MuiChipsInput } from 'mui-chips-input'
+import React, { useState } from "react";
+import { MuiChipsInput } from "mui-chips-input";
 import nextId from "react-id-generator";
-import "./ADDTOPICS.scss"
+import "./ADDTOPICS.scss";
 
 const ADDTOPICS = ({ setTopics }) => {
+  const [formData, setFormData] = useState({
+    id: "",
+    title: "",
+    keyword: [],
+    category: "custom",
+    content: "",
+    tone: "",
+    img: "",
+  });
 
-    const [formData, setFormData] = useState(
+  const handelField = (e) => {
+    setFormData((pre) => {
+      return {
+        ...pre,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+  const handelOnSubmit = (e) => {
+    e.preventDefault();
+    //workspace
+    setTopics((pre) => {
+      return [
+        ...pre,
         {
-            id: "",
-            title: "",
-            keyword: [],
-            category: "custom",
-            content: "",
-            tone: "",
-            img: ""
-        }
-    )
+          ...formData,
+          id: nextId(),
+        },
+      ];
+    });
+    console.log(formData);
 
-    const handelField = (e) => {
+    setFormData({
+      id: "",
+      title: "",
+      keyword: [],
+      category: "custom",
+    });
+  };
+  const handleChange = (newChips) => {
+    setFormData((pre) => {
+      return {
+        ...pre,
+        keyword: newChips,
+      };
+    });
+  };
+  return (
+    <div className="ADDTOPICS">
+      <div className="container">
+        <div className="form">
+          <label htmlFor="titel">Title</label>
+          <input
+            name="title"
+            id="titel"
+            value={formData.title}
+            type="text"
+            placeholder="Enter title"
+            onChange={(e) => handelField(e)}
+          />
+          <label className="mar" htmlFor="keywords">
+            KeyWords
+          </label>
 
-        setFormData((pre) => {
-            return {
-                ...pre,
-                [e.target.name]: e.target.value,
+          <MuiChipsInput
+            id="keywords"
+            value={formData.keyword}
+            onChange={(newChips) => handleChange(newChips)}
+          />
+          <button className="button" onClick={(e) => handelOnSubmit(e)}>
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-            }
-        })
-    }
-    const handelOnSubmit = (e) => {
-        e.preventDefault()
-        //workspace
-        setTopics((pre) => {
-            return [
-                ...pre,
-                {
-                    ...formData,
-                    id: nextId()
-                },
-
-            ]
-        })
-        console.log(formData);
-
-        setFormData({
-            id: "",
-            title: "",
-            keyword: [],
-            category: "custom"
-        })
-
-
-
-    }
-    const handleChange = (newChips) => {
-        setFormData((pre) => {
-            return {
-                ...pre,
-                keyword: newChips
-            }
-        })
-
-    }
-    return (
-        <div className='ADDTOPICS'>
-            <div className="container">
-                <form onSubmit={(e) => handelOnSubmit(e)}>
-                    <label htmlFor="titel">Title</label>
-                    <input name="title" id='titel' value={formData.title} type="text" placeholder='Enter title' onChange={(e) => handelField(e)} />
-                    <label className='mar' htmlFor="keywords">KeyWords</label>
-                    {/* css -ye bilkul bekar dikh raha h */}
-                    <MuiChipsInput id='keywords' value={formData.keyword} onChange={(newChips) => handleChange(newChips)} />
-                    <button className='button'>Submit</button>
-                </form>
-            </div>
-
-        </div >
-    )
-}
-
-export default ADDTOPICS
+export default ADDTOPICS;
